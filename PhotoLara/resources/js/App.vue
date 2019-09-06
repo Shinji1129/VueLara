7bb1a1f86c212589eb18711f9813c3abb2128f78
Shinji Kawamoto <viii49ix40x35@gmail.com>
2019-09-06 11:54:51 +0900

    認証ページUI

--- PhotoLara/resources/js/App.vue
+++ PhotoLara/resources/js/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <header>
-      <Navibar />
+      <Navbar />
     </header>
     <main>
       <div class="container">
@@ -13,12 +13,12 @@
 </template>
 
 <script>
-import Navibar from './components/Navibar.vue'
+import Navbar from './components/Navbar.vue'
 import Footer from './components/Footer.vue'
 
 export default {
   components: {
-    Navibar,
+    Navbar,
     Footer
   }
 }
